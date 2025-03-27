3e9e34ffc0f3eac705b393ce1043e6026b2e62ad
Vaibhav Raj <87658762+vaibhav42069@users.noreply.github.com>
2025-03-27 18:02:39 +0530

    Update proc_load_silver.sql

--- script/silver/proc_load_silver.sql
+++ script/silver/proc_load_silver.sql
@@ -60,3 +60,50 @@ select
 	AS prd_end_dt
 from [bronze].[crm_prd_info]	
 
+--------------------------------------------------------------------------------------------------------
+
+
+INSERT INTO silver.crm_sales_details (
+			sls_cust_id,
+			sls_prd_key,
+			sls_order_dt,
+			sls_ship_dt,
+			sls_due_dt,
+			sls_ord_num,
+			sls_sales,
+			sls_quantity,
+			sls_price
+)
+		
+  select	        
+	sls_cust_id,
+	sls_prd_key,
+	case 
+	when sls_order_dt =0 or len(sls_order_dt)!=8 then null
+	else cast(cast(sls_order_dt as varchar) as date)
+	end as sls_order_dt,
+	case
+	when sls_ship_dt =0 or len(sls_ship_dt)!=8 then null
+	else cast(cast(sls_ship_dt as varchar) as date)
+	end as sls_ship_dt,
+	case
+	when sls_due_dt =0 or len(sls_due_dt)!=8 then null
+	else cast(cast(sls_due_dt as varchar) as date)
+	end as sls_due_dt,
+	sls_ord_num,
+	case 
+    when sls_sales is null or sls_sales <=0 or sls_sales!=sls_quantity*ABS(sls_price) 
+    then sls_quantity*ABS(sls_price) 
+    else sls_sales
+    end as sls_sales,
+	sls_quantity,
+	case 
+	when sls_price is null or sls_price <=0  
+	then sls_sales/nullif(sls_quantity,0)
+	else sls_price
+	end as sls_price
+
+	From bronze.crm_sales_details
+-------------------------------------------------------------------------------------------------
+
+
