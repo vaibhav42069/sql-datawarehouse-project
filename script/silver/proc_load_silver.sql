e9dfe549d60549b017a53eb5537654c3c39427cd
Vaibhav Raj <87658762+vaibhav42069@users.noreply.github.com>
2025-03-28 12:37:55 +0530

    Update proc_load_silver.sql

--- script/silver/proc_load_silver.sql
+++ script/silver/proc_load_silver.sql
@@ -142,3 +142,17 @@ select replace (cid, '-','')as cid,
 from [bronze].[erp_loc_a101]
 
 --------------------------------------------------------------------------------------------------------
+
+INSERT INTO silver.erp_px_cat_g1v2 (
+			id,
+			cat,
+			subcat,
+			maintenance
+		)
+		SELECT
+			id,
+			cat,
+			subcat,
+			maintenance
+		FROM bronze.erp_px_cat_g1v2;
+
