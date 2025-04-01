a80cb06ef11c9bc343f30288c6c409905d7758db
Vaibhav Raj <87658762+vaibhav42069@users.noreply.github.com>
2025-04-01 12:55:15 +0530

    Update and rename dd_gold.sql to ddl_gold.sql

--- script/Gold/ddl_gold.sql
+++ script/Gold/ddl_gold.sql
@@ -19,3 +19,24 @@ as
 	on  ci.cst_key=ca.cid
 	left join [silver].[erp_loc_a101] la
 	on  ci.cst_key=la.cid 
+-----------------------------------------------------------------------------------------------------------------
+
+create view gold.dim_products 
+as
+select
+    ROW_NUMBER() over(order by prd_key,prd_start_dt) as product_key,
+	pn.prd_id as product_id,
+	pn.prd_key as product_number,
+	pn.prd_nm as product_name,
+	pn.cat_id as category_id,
+	pc.cat as category,
+	pc.subcat subcategory,
+	pc.maintenance,
+	pn.prd_cost as product_cost,
+	pn.prd_line as product_line,
+	pn.prd_start_dt as start_date
+from silver.crm_prd_info pn
+left join silver.erp_px_cat_g1v2 pc 
+on  pn.cat_id=pc.id   
+where prd_end_dt is null
+
